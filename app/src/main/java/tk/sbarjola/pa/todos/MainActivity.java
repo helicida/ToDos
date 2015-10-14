448f1aa5571feca2f6b71d17cec54875cdbe9cf0
Sergi <sergibca@gmail.com>
2015-10-14 20:39:02 +0200

    ArrayAdapter y metodo onClick implementados. Algunos objetos han pasado a ser globales. Existen errores a solucionar.

--- app/src/main/java/tk/sbarjola/pa/todos/MainActivity.java
+++ app/src/main/java/tk/sbarjola/pa/todos/MainActivity.java
@@ -4,14 +4,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> items;
+    ArrayAdapter myAdapter;
+    ListView lista = (ListView) findViewById(R.id.listView); // Lista
+    Button boton = (Button) findViewById(R.id.button); //Botón de añadido
+    EditText texto = (EditText) findViewById(R.id.editText); //EditText
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();  //Array list
+        myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);    //ArrayAdapter
+        lista.setAdapter(myAdapter);    //Acoplamos el array al listView a través del adapter
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {//Hacemos el listener
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                myAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +64,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view) {
+        myAdapter.add((String.valueOf(texto.getText())));
+        texto.setText("");
     }
 }
